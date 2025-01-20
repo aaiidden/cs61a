d6306a7167209e2f29f9eec08332d3924620b0bc
Aiden Wei <aiden.wei.cn@outlook.com>
2025-01-20 17:41:29 +0800

    complete lab09

--- lab09/lab09.scm
+++ lab09/lab09.scm
@@ -1,10 +1,14 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) (cond ((< num1 num2) -1)
+                                          ((= num1 num2) 0)
+                                          ((> num1 num2) 1)))
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num) (lambda (inc) (+ inc num)))
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g) (lambda (x) (f (g x))))
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (repeat f n) (if (= n 1)
+  (lambda (x) (f x))
+  (lambda (x) ((composed (repeat f (- n 1)) f) x))))
 
 (define (max a b)
   (if (> a b)
@@ -16,4 +20,7 @@
       b
       a))
 
-(define (gcd a b) 'YOUR-CODE-HERE)
+(define (gcd a b) (let ((x (max a b)) (y (min a b)))
+  (if (zero? (modulo x y))
+    y
+    (gcd y (modulo x y)))))
